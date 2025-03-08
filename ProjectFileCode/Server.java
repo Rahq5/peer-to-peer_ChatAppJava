9899a3ddc3787218907e357fe82e41cc248531a2
Rahq5 <rawihk2005@gmail.com>
2025-03-08 03:46:59 +0300

    app can send messades now !!

--- ProjectFileCode/Server.java
+++ ProjectFileCode/Server.java
@@ -5,11 +5,30 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Server {
+    private ServerSocket serverSocket;
+
+    public Server(ServerSocket serverSocket){
+        this.serverSocket = serverSocket;
+    }
     public static void main(String[] args) throws IOException{
         
         System.out.println("Server is waiting for clients");
-        ServerSocket ss = new ServerSocket(1111);
-        Socket socket = ss.accept();
-        System.out.println("a new client joined the server !");
+        ServerSocket ss = new ServerSocket(1111); // serversocket "listener" is made to listen on port 1111
+        Server server = new Server(ss);// server have the listener to start functioing
+        server.run(); 
+    }
+
+    public void run(){ //here where listener is accpeting responds and passes them to clienthandler
+       try{
+        while(!serverSocket.isClosed()){
+
+            Socket socket = serverSocket.accept();
+            System.out.println("new client joined"); //add repreasenting name on server.java output 
+            ClientHandler clientHandler = new ClientHandler(socket);
+            Thread thread = new Thread(clientHandler);
+            thread.start();
+        }
+        }catch(IOException e){e.printStackTrace();}
+        
     }
 }
--- README.md
+++
@@ -1,7 +0,0 @@
-#peer-to-perr ChatApp JAVA
-this is a chat app java-based project that uses synchronized client server to communicate using java sockets
-
-#Featuers:
-- **real time messaging** : all clients joined can read\send messages instantly
-- **global**: any client can sign their names and start chatting
-- coming soon...
